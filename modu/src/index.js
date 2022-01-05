8d07b281f184835b2ef404971aad872bd4d6757c
younggyulee <baayoo93@naver.com>
2022-01-05 15:27:40 +0900

    Chore: Redux 초기설정

--- modu/src/index.js
+++ modu/src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import ReduxThunk from "redux-thunk";
+import logger from "redux-logger";
+import { BrowserRouter } from "react-router-dom";
+import rootReducer from "./modules";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger)),
+);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root"),
+);
